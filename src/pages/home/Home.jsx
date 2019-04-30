b20d4683e25026483b3987d3f6bb1010d3899ef1
vebjorngronhaug <vebjorn.gronhaug@gmail.com>
2019-05-01 00:47:36 +0200

    Installed react-responsive - Implemented in Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Menu from "../components/menu/Menu";
+import MediaQuery from 'react-responsive';
 import "./Home.css";
 import "../pageStyle.css";
 
@@ -7,9 +8,10 @@ class Home extends Component {
     render(){
         return (
             <div>
+                <MediaQuery minWidth="900px">
                 <div className="split left">
                     <div className="centerMenu">
-                        <Menu />
+                        <Menu />                
                     </div>
                 </div>
                 <div className="split right homeColor"> 
@@ -18,6 +20,14 @@ class Home extends Component {
                         Click on the menu to learn more about the mobile lifecycle!
                     </div> 
                 </div>
+                </MediaQuery>
+                <MediaQuery maxWidth={899}>
+                    <div className="mobilePage">
+                        <div className="mobileSize">
+                            Oops! Det ser ut til at du er på en mobil enhet. For å se menyen må skjermen være større enn 900px bred!
+                        </div>
+                    </div>
+                </MediaQuery>
             </div>
         );
     }
