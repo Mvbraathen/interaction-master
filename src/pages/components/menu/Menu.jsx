30aa671568dd8ef8928c2c0a6013fe5448752659
vebjorngronhaug <vebjorn.gronhaug@gmail.com>
2019-04-30 20:32:22 +0200

    installed breadcrumbs

--- src/pages/components/menu/Menu.jsx
+++ src/pages/components/menu/Menu.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import "./Menu.css";
 import ResourceExtraction from "./components/resourceExtraction/ResourceExtraction";
 import Manufacturing from "./components/manufacturing/Manufacturing";
 import Distribution from "./components/distribution/Distribution";
@@ -7,10 +6,11 @@ import Use from "./components/use/Use";
 import EndOfLife from "./components/endOfLife/EndOfLife";
 import Design from "./components/design/Design";
 import CenterObjects from "./components/centerObjects/CenterObjects";
+import "./Menu.css";
 
 class Menu extends Component {        
     render(){
-        return <div className="circleMenu">
+        return <div>
             <svg width="430" height="430">
                 <g>
                     <ResourceExtraction />              
