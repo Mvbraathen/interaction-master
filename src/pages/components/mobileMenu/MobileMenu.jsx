78337b1301fb178b8f415a949f9520d315bd4321
vebjorngronhaug <vebjorn.gronhaug@gmail.com>
2019-05-02 12:33:30 +0200

    added color when active effect on mobile menu

--- src/pages/components/mobileMenu/MobileMenu.jsx
+++ src/pages/components/mobileMenu/MobileMenu.jsx
@@ -6,29 +6,20 @@ import {
 import './MobileMenu.css';
 
 class Menu extends Component {  
-    showSettings (event) {
-    event.preventDefault();
-
-  }      
+       
     render(){
         return (
             <div>
                 <BurgerMenu>
-                    <div className = "bm-item-list">
-                        <NavLink className="bm-item" to ="/"> Home </NavLink>
-                        <br/>
-                        <NavLink className="bm-item" to ="/design"> Design </NavLink>
-                        <br/>
-                        <NavLink className="bm-item" to ="/resource-extraction"> Resource Extraction </NavLink>
-                        <br/>
-                        <NavLink className="bm-item" to ="/manufacturing"> Manufacturing </NavLink>
-                        <br/>
-                        <NavLink className="bm-item" to ="/distribution"> Distribution </NavLink>
-                        <br/>
-                        <NavLink className="bm-item" to ="/use"> Use </NavLink>
-                        <br/>
-                        <NavLink className="bm-item" to ="/end-of-life"> End of Life </NavLink>
-                    </div>
+                    <ul className = "bm-item-list">
+                        <li><NavLink className="bm-item" to ="/start"> Home </NavLink></li>
+                        <li><NavLink className="bm-item" to ="/design"> Design </NavLink></li>
+                        <li><NavLink className="bm-item" to ="/resource-extraction"> Resource Extraction </NavLink></li>
+                        <li><NavLink className="bm-item" to ="/manufacturing"> Manufacturing </NavLink></li>
+                        <li><NavLink className="bm-item" to ="/distribution"> Distribution </NavLink></li>
+                        <li><NavLink className="bm-item" to ="/use"> Use </NavLink></li>
+                        <li><NavLink className="bm-item" to ="/end-of-life"> End of Life </NavLink></li>
+                    </ul>
                 </BurgerMenu>
             </div>
         );
