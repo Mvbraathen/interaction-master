33f77981ed6184e3c4359932a6616d619ebb3ae7
vebjorngronhaug <vebjorn.gronhaug@gmail.com>
2019-05-06 13:23:48 +0200

    fixed MediaQuery for large screens

--- src/application/pages/use/pages/default/Default.jsx
+++ src/application/pages/use/pages/default/Default.jsx
@@ -13,7 +13,7 @@ class Use extends Component {
     render(){
         return (
             <div>
-                <MediaQuery minWidth="850px">
+                <MediaQuery minWidth="850px" maxWidth="1199px">
                     <div className="split left">
                         <div className ="breadcrumb">
                             <NavLink to ="/home"><div className="prevPage">Home</div></NavLink>
@@ -30,6 +30,29 @@ class Use extends Component {
                         </div> 
                     </div>
                 </MediaQuery>
+
+
+                <MediaQuery minWidth="1200px">
+                    <div className="split leftBig">
+                        <div className ="breadcrumb">
+                            <NavLink to ="/home"><div className="prevPage">Home</div></NavLink>
+                            <div className="breadcrumbSpacing"> > </div>
+                            <NavLink to ="/use"><div className="activePage">Use</div></NavLink>
+                        </div>
+                        <Desktop />
+                    </div>
+                    <div className="split rightBig"> 
+                        <div className="bigDefaultContent">
+                            <h1> Use </h1>
+                            <div> Du er nå på Use-siden! </div>
+                            <NavLink to ="/use/lifetime"><div className="lifetimeLink">Go to Lifetime</div></NavLink>
+                        </div> 
+                    </div>
+                </MediaQuery>
+
+            
+
+
                 <MediaQuery maxWidth={849}>
                     <div className="mobilePage">
                         <div className="mobileContent">
