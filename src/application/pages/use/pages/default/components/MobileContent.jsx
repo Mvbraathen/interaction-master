39ba26bd30ca3e0cc9ec0cd7ebd90ddd68f70967
vebjorngronhaug <vebjorn.gronhaug@gmail.com>
2019-05-10 15:52:31 +0200

    design fixes on scroll snap

--- src/application/pages/use/pages/default/components/MobileContent.jsx
+++ src/application/pages/use/pages/default/components/MobileContent.jsx
@@ -4,8 +4,32 @@ import {
     NavLink
 } from "react-router-dom";
 import "../../../../css/MobilePageStyle.css";
+import ScrollSnap from 'scroll-snap';
+
+const snapConfig = {
+  scrollSnapDestination: "0% 90%",
+  scrollTimeout: 100,
+  scrollTime: 300
+};
 
 class MobileContent extends React.Component {
+    container = React.createRef();
+
+    bindScrollSnap() {
+        function callback() {
+          // optional callback
+        }
+
+        const element = this.container.current;
+        const snapObject = new ScrollSnap(element, snapConfig);
+        snapObject.bind(callback);
+    }
+
+    componentDidMount() {
+        this.bindScrollSnap();
+    }
+
+
 	render() {
 		return (
 			<div>
@@ -15,7 +39,98 @@ class MobileContent extends React.Component {
                         <div className="pageHeaderBigScreen">  
                             <h1> Use </h1>
                         </div>
-                            <div className="pappa-salah">
+                        <div id="container" ref={this.container}>
+                            <div className="page first-page">
+                                <div className="page-one-header">
+                                    This is a header
+                                </div>
+                                <img
+                                    alt="A road "
+                                    className="page-one-image" 
+                                    src={require('./images/smart-test-image.jpg')} 
+                                />
+                                <div className="page-one-info"> 
+                                    More than half of the world population uses mobile 
+                                    phone today. The popularity of mobile phones has lead to significant environmental 
+                                    risk in the form of CO2 emissi... 
+                                    <NavLink to="/use/lifetime">
+                                        <button className="page-one-button"> Read more </button>
+                                    </NavLink>
+                                </div>
+                                <div className="hint">
+                                    Scroll down for next element <span>&darr;</span>
+                                </div>
+                            </div>
+                            <div className="page second-page">
+                                <div className="page-one-header">
+                                    This is a header
+                                </div>
+                                <img
+                                    alt="A road "
+                                    className="page-one-image" 
+                                    src={require('./images/smart-test-image.jpg')} 
+                                />
+                                <div className="page-one-info"> 
+                                    More than half of the world population uses mobile 
+                                    phone today. The popularity of mobile phones has lead to significant environmental 
+                                    risk in the form of CO2 emissi... 
+                                    <NavLink to="/use/lifetime">
+                                        <button className="page-one-button"> Read more </button>
+                                    </NavLink>
+                                </div>
+                                <div className="hint">
+                                    Scroll down for next element <span>&darr;</span>
+                                </div>
+                            </div>
+                            <div className="page third-page">
+                                <div className="page-one-header">
+                                    This is a header
+                                </div>
+                                <img
+                                    alt="A road "
+                                    className="page-one-image" 
+                                    src={require('./images/smart-test-image.jpg')} 
+                                />
+                                <div className="page-one-info"> 
+                                    More than half of the world population uses mobile 
+                                    phone today. The popularity of mobile phones has lead to significant environmental 
+                                    risk in the form of CO2 emissi... 
+                                    <NavLink to="/use/lifetime">
+                                        <button className="page-one-button"> Read more </button>
+                                    </NavLink>
+                                </div>
+                                <div className="hint">
+                                    Scroll down for next element <span>&darr;</span>
+                                </div>
+                            </div>
+                            <div className="page fourth-page">
+                                <div className="page-one-header">
+                                    This is a header
+                                </div>
+                                <img
+                                    alt="A road "
+                                    className="page-one-image" 
+                                    src={require('./images/smart-test-image.jpg')} 
+                                />
+                                <div className="page-one-info"> 
+                                    More than half of the world population uses mobile 
+                                    phone today. The popularity of mobile phones has lead to significant environmental 
+                                    risk in the form of CO2 emissi... 
+                                    <NavLink to="/use/lifetime">
+                                        <button className="page-one-button"> Read more </button>
+                                    </NavLink>
+                                </div>
+                                <div className="hint">
+                                    Scroll up to go back <span>&uarr;</span>
+                                </div>
+                            </div>
+                          </div>
+                            
+
+
+
+
+                            {/*<div className="pappa-salah">
                                 <div className="salah">
                                     <div className="salah-header"> Acidification </div>
                                     <img className="salah-image" src={require('./images/smart-test-image.jpg')} />
@@ -66,7 +181,7 @@ class MobileContent extends React.Component {
                                     <NavLink to="/use/lifetime">
                                         <button className="salah-button"> Learn more </button>
                                     </NavLink>
-                                </div>                                
+                                </div> */}                               
 
 
 
@@ -131,9 +246,8 @@ class MobileContent extends React.Component {
                                     <div className="hacky-flex-fix">  
                                     </div>
                                 </div>
-                                */}
-
                             </div>
+                            */}
                         </div>
                         <Mobile />
                     </div>
