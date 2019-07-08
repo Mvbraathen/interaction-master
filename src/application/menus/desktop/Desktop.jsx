32512695fa21342770876d14dec972ead257df4d
vebjorngronhaug <vebjorn.gronhaug@gmail.com>
2019-07-08 04:07:53 +0200

    typo

--- src/application/menus/desktop/Desktop.jsx
+++ src/application/menus/desktop/Desktop.jsx
@@ -18,8 +18,8 @@ class Menu extends Component {
     if(this.props.fromPage === 'home') {
       explainMenu = (
         <g>
-          <text text-anchor="middle" x="212.5" y="-40" fontSize="18" fill="black">To learn more, interact</text>
-          <text text-anchor="middle" x="212.5" y="-20" fontSize="18" fill="black">with the circular menu below!</text>
+          <text textAnchor="middle" x="212.5" y="-40" fontSize="18" fill="black">To learn more, interact</text>
+          <text textAnchor="middle" x="212.5" y="-20" fontSize="18" fill="black">with the circular menu below!</text>
         </g>
       )
     }
