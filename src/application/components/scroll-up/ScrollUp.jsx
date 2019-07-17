b4ca2080a01741df97d4c146ac7ae5f3b71c4fb8
vebjorngronhaug <vebjorn.gronhaug@gmail.com>
2019-07-17 11:38:55 +0200

    minor

--- src/application/components/scroll-up/ScrollUp.jsx
+++ src/application/components/scroll-up/ScrollUp.jsx
@@ -7,11 +7,20 @@ class ScrollUp extends React.Component {
         window.scrollTo(0, 0);
     }
 
+    /* Making scroll up accessible to keyboard users */
+    handleKeyPress = (event) => {
+        if(event.key === 'Enter'){
+            this.handleScrollTop()
+        }
+    }
+
 	render() {
         return (
             <div
+                role="button"
                 title="Jump to top of page"
                 onClick={this.handleScrollTop}
+                onKeyPress={this.handleKeyPress}
                 tabIndex="0" 
                 className="return-to-top-button"> 
                 <span role="img" aria-label="finger pointing upwards"> ☝️ </span> 
